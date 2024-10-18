60caa96accd9ceea0eee3a0035cff818a3290a3d
elviredev <sandrine.rodriguez@hotmail.com>
2024-10-18 13:06:58 +0200

    � Initialize Project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>Hello, Welcome to my Laravel app</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ea facere illum impedit natus quia sed. Adipisci alias amet dicta distinctio libero minima nobis numquam obcaecati praesentium, rerum similique temporibus?</p>
